0cfc5ee0a48120a6353033d4897f42517169ebf1
Brainboard <no-reply@brainboard.co>
2023-08-05 18:51:46 +0000

    Brainboard update

--- providers.tf
+++ providers.tf
@@ -1,3 +1,4 @@
-provider "aws" {
-  region = "var.region"
+terraform {
+  required_providers {
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,45 +1,5 @@
-variable "region" {
-  description = "The AWS region"
-  type        = string
-  default     = "us-west-2"
+variable "tags" {
+  description = "Default tags to apply to all resources."
+  type        = map(any)
 }
 
-variable "pipeline_name" {
-  description = "The name of the pipeline"
-  type        = string
-}
-
-variable "pipeline_role_arn" {
-  description = "The ARN of the IAM role for the pipeline"
-  type        = string
-}
-
-variable "artifact_store_location" {
-  description = "The location of the artifact store"
-  type        = string
-}
-
-variable "source_bucket" {
-  description = "The S3 bucket for the source stage"
-  type        = string
-}
-
-variable "source_object_key" {
-  description = "The S3 object key for the source stage"
-  type        = string
-}
-
-variable "build_project_name" {
-  description = "The name of the CodeBuild project for the build stage"
-  type        = string
-}
-
-variable "app_name" {
-  description = "The name of the CodeDeploy application for the deploy stage"
-  type        = string
-}
-
-variable "deployment_group_name" {
-  description = "The name of the CodeDeploy deployment group for the deploy stage"
-  type        = string
-}
